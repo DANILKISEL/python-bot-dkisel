35af4efde137c5b018004e9ac78bdd8c36235bfa
DanilBot-ui <grown.dk.up@gmail.com>
2025-09-28 12:11:36 +0300

    update

--- botStarter.py
+++ botStarter.py
@@ -1,10 +1,11 @@
-import telebot
+from telebot import TeleBot
 
 class Bot:
     def __init__(self, botname):
-        self.tokenFile = open("token.txt", "r")
+        self.tokenFile = open("./token.txt", "r")
         self.token= self.tokenFile.read()
-        self.bot = telebot.TeleBot(self.token)
+        self.defalutToken = "8472252626:AAEZnA6dXpxa-2NWoRuJ-S3FtiJxP9JqlAg"
+        self.bot = TeleBot(self.defalutToken)
         self.botName = botname
 
     def get_bot(self):
--- tictacto-bot.py
+++ tictacto-bot.py
@@ -1,46 +1,48 @@
 import random
-
 from botStarter import Bot
-bot = Bot("bot").get_bot()
 from telebot import types
 
-@bot.message_handler(commands=['game'])
-def game(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-
-    itm1 = types.KeyboardButton("Камень")
-    itm2 = types.KeyboardButton("Ножницы")
-    itm3 = types.KeyboardButton("Бумага")
-    markup.add(itm1, itm2, itm3)
-    bot.send_message(message.chat.id, "Choose a game", reply_markup=markup)
+bot = Bot("bot").get_bot()
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton("/game")
     markup.add(button)
-    bot.send_message(message.chat.id, "Hello! Lets start a game:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Hello! Let's start a game:", reply_markup=markup)
 
+@bot.message_handler(commands=['game'])
+def game(message):
+    markup = types.InlineKeyboardMarkup(row_width=3)
+
+    # Use inline buttons with callback_data matching English options
+    itm1 = types.InlineKeyboardButton(text="Камень", callback_data="Rock")
+    itm2 = types.InlineKeyboardButton(text="Ножницы", callback_data="Scissors")
+    itm3 = types.InlineKeyboardButton(text="Бумага", callback_data="Paper")
+    markup.add(itm1, itm2, itm3)
+
+    bot.send_message(message.chat.id, "Choose a move:", reply_markup=markup)
 
 answer_pool = ["Rock", "Paper", "Scissors"]
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-    bot_answer = answer_pool[random.randint(0, 2)]
-    bot.send_message(call.message.chat.id, bot_answer)
-    result = answer_pool.index(call.data) - answer_pool.index(bot_answer)
-    if result == -1 or result == 2:
+    bot_answer = random.choice(answer_pool)
+    user_choice = call.data
+
+    # Send bot's move to user
+    bot.send_message(call.message.chat.id, f"Bot chooses: {bot_answer}")
+
+    # Determine result
+    result = (answer_pool.index(user_choice) - answer_pool.index(bot_answer)) % 3
+
+    if result == 1:
         message = "YOU WIN"
-    elif result == 0:
-        message = "TIE"
-    else:
+    elif result == 2:
         message = "YOU LOSE"
+    else:
+        message = "TIE"
 
     bot.send_message(call.message.chat.id, message)
 
-
-
-
 bot.infinity_polling()
-
-
-
